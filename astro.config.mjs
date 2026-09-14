98d1bad7dbdd21a29d5642cf500263476b76deaa
PeDitX <peditx@PeDitXs-MacBook-Pro.local>
2026-09-14 20:14:31 +0330

    Fix: prevent cloudflare adapter auto-detection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://tizone.ir',
   output: 'static',
+  // Prevent Cloudflare from auto-adding adapter
+  server: { host: true },
 });
